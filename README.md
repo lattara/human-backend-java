f8dee679bdcecea5fa3e6d75197ea439076c1650
lattara <atopic.ie@gmail.com>
2021-05-21 14:37:55 +0200

    adding swagger and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# human-backend-java
+# Human Back end JAVA Spring Boot
+
+Java backend for human e-commerce site
+to install dependencies
+mvn install
+mvn update
+
+Documentation - swagger - 
+http://localhost:8080/swagger-ui/
+
+Documentation -swagger - JSON
+http://localhost:8080/v2/api-docs
--- src/main/java/com/human/HumanApp.java
+++ src/main/java/com/human/HumanApp.java
@@ -1,12 +1,13 @@
 package com.human;
 
-import com.human.users.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableJpaRepositories ()
+@EnableJpaRepositories
+@EnableSwagger2
 public class HumanApp {
 
 	public static void main(String[] args) {
