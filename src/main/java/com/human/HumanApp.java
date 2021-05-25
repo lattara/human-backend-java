5b96f6060a28657a854e4432ebcdf94cdeb9d2ac
lattara <atopic.ie@gmail.com>
2021-05-25 14:41:39 +0200

    setting up different profiles

--- src/main/java/com/human/HumanApp.java
+++ src/main/java/com/human/HumanApp.java
@@ -1,8 +1,11 @@
 package com.human;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
--- src/main/java/com/human/pages/PagesController.java
+++ src/main/java/com/human/pages/PagesController.java
@@ -1,12 +1,20 @@
 package com.human.pages;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 
 public class PagesController {
+
+    @Autowired
+    private Environment env;
 
     // TODO delete this class later, it is only for learning / testing purposes
 
@@ -18,6 +26,11 @@ public class PagesController {
     @RequestMapping("/admin")
     public String admin() {
         return "<h1> Welcome to backoffice admin </h1>";
+    }
+
+    @RequestMapping("/env")
+    public String env() {
+        return env.toString();
     }
 
 }
