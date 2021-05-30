9a62ff332cfb4d709045303a61520c4141de8388
lattara <atopic.ie@gmail.com>
2021-05-30 10:51:05 +0200

    in progress

--- src/main/java/com/human/users/MyUserDetailsService.java
+++ src/main/java/com/human/users/MyUserDetailsService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class   MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
